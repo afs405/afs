969d64e3b980fdb83a0d574f41d8af050ed20e75
afs405 <90897600+afs405@users.noreply.github.com>
2023-10-16 14:51:57 +0100

    Added emailing capability

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,24 @@
-fName = document.getElementById("fName");
-email = document.getElementById("email");
-message = document.getElementById("message");
+emailjs.init("KRXzwPpKPGq8Y13bW");
+
+function sendEmail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs
+    .send(
+      "afs405@gmail.com",
+      "emailjs.send(serviceID, templateID, templateParams, publicKey);",
+      params
+    )
+    .then(
+      function (response) {
+        console.log("SUCCESS!", response.status);
+      },
+      function (error) {
+        console.log("ERROR!", error);
+      }
+    );
+}
